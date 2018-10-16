337d85a0391215c189b54edff591fa77e437eb5b
rocco <rocco.mormont@gmail.com>
2018-10-16 23:45:59 +0800

    :bento: need to fix; assets path is wrong

--- sub-app-one/src/views/PageA.vue
+++ sub-app-one/src/views/PageA.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="page">
     <h3>This is a page: A</h3>
+
+    <img class="image"
+         src="@/assets/images/nodejs.png"
+         alt="nodejs">
+
+    <div class="background"></div>
   </div>
 </template>
+
+<style lang="less">
+.page {
+  .image {
+    width: 40vmin;
+  }
+
+  .background {
+    width: 20vmin;
+    height: 20vmin;
+    background: url('~@/assets/images/javascript.png') no-repeat;
+    background-size: cover;
+  }
+}
+</style>

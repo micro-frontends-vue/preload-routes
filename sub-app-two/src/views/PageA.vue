2a35610accd916606e3d9846c7b554e22d098a72
rocco <rocco.mormont@gmail.com>
2018-10-19 09:29:19 +0800

    error example: assets path is wrong

--- sub-app-two/src/views/PageA.vue
+++ sub-app-two/src/views/PageA.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="page">
     <h3>This is a page: A</h3>
+
+    <img class="image"
+         src="@/assets/images/nodejs.png"
+         alt="nodejs">
+    <br>
+    <p>Open the Chrome DevTools, there is an image loading error:</p>
+    <div class="background"></div>
   </div>
 </template>
+
+<style lang="less">
+.page {
+  .image {
+    width: 40vmin;
+  }
+
+  .background {
+    display: inline-flex;
+    width: 20vmin;
+    height: 20vmin;
+    background: #ea6e76 url('~@/assets/images/javascript.png') no-repeat;
+    background-size: cover;
+  }
+}
+</style>
